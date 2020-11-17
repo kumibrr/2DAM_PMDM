4cb47aa9b2fc685ce000e18f1304183ac045ab22
kumibrr <isra.lora99@gmail.com>
2020-11-17 18:03:32 +0100

    Ejercicio formulario chuck

---
+++ 004/model-driven-form/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { JokeComponent } from './components/joke/joke.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    JokeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
